86fb69fa06b5c34c2fa6d06bca675167a7e027ec
Burakkturk <burak1845turk@icloud.com>
2024-08-03 11:13:29 +0300

    box added

--- src/components/Typo.jsx
+++ src/components/Typo.jsx
@@ -1,9 +1,17 @@
+import { Padding } from '@mui/icons-material'
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const Typo = () => {
   return (
-    <div>Typo</div>
-  )
+    <Box sx={{backgroundColor:"red", padding:"1 rem"}}>
+        <Typography variant="h6" component="h1" color="#bebe">MUI Typo</Typography>
+        <Typography variant="subtitle2"  color="palegreen">MUI Typo</Typography>
+        <Typography variant="body2" >Button Text</Typography>
+        <Typography variant="body2" sx={{color:"blue", backgroundColor:"palevioletred", mt:"1rem"}} >Button Text</Typography>
+        
+        Typo</Box>
+  ) 
 }
 
 export default Typo
